94f7113fabd9b7b931ebd76be530623b17aacf8e
Marco Petruccelli <petru.map@gmail.com>
2026-04-16 22:10:12 -0300

    musica lista

--- scripts/levels/level_2_manager.gd
+++
@@ -1,9 +0,0 @@
-# scripts/levels/level_2_manager.gd
-extends Node
-
-func _ready() -> void:
-	
-	# Reproducir música
-	if MusicManager:
-		MusicManager.play_music("res://assets/audio/music/LEVEL 2.wav", true)
-	return
